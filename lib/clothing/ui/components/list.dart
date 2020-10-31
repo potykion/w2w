5ea2dd79d7140b521241e32206461440901c2c5b
potykion <potykion@gmail.com>
2020-10-31 13:12:58 +0300

    TypeClothingList

--- lib/clothing/ui/components/list.dart
+++ lib/clothing/ui/components/list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:w2w/clothing/domain/models.dart';
 
 class ClothingListAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
@@ -20,4 +21,40 @@ class ClothingListAppBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
+
+class TypeClothingList extends StatelessWidget {
+  final List<Clothing> clothingList;
+
+  const TypeClothingList({Key key, this.clothingList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+        height: 260,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    clothingList[index].images.first,
+                    height: 200,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    clothingList[index].title,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  )
+                ],
+              ),
+            ),
+          ),
+          itemCount: clothingList.length,
+        ),
+      );
 }
--- lib/clothing/ui/pages/list.dart
+++ lib/clothing/ui/pages/list.dart
@@ -4,75 +4,40 @@ import 'package:get/get.dart';
 import 'package:w2w/clothing/state/controllers.dart';
 import 'package:w2w/clothing/ui/components/list.dart';
 
-
-
 class ClothingListPage extends GetView<ClothingListController> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: controller.clothingList.length == 0
-          ? buildNoClothing(context)
-          : buildGroupedClothingList(context),
-      appBar: ClothingListAppBar(),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: controller.clothingList.length == 0
+            ? buildNoClothing(context)
+            : buildGroupedClothingList(context),
+        appBar: ClothingListAppBar(),
+      );
 
-  Center buildNoClothing(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "В гардеробе пустовато\nДобавь что-нибудь",
-            style: Theme.of(context).textTheme.headline4,
-            textAlign: TextAlign.center,
-          )
-        ],
-      ),
-    );
-  }
+  Center buildNoClothing(BuildContext context) => Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "В гардеробе пустовато\nДобавь что-нибудь",
+              style: Theme.of(context).textTheme.headline4,
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      );
 
-  buildGroupedClothingList(BuildContext context) {
-    return ListView(
-      children: controller.groupedClothing.entries
-          .expand((entry) => [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Тип: ${entry.key}",
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                ),
-                SizedBox(
-                  height: 260,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) => Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 150,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.network(
-                              entry.value[index].images.first,
-                              height: 200,
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              entry.value[index].title,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 2,
-                            )
-                          ],
-                        ),
-                      ),
+  buildGroupedClothingList(BuildContext context) => ListView(
+        children: controller.groupedClothing.entries
+            .expand((entry) => [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Тип: ${entry.key}",
+                      style: Theme.of(context).textTheme.headline6,
                     ),
-                    itemCount: entry.value.length,
                   ),
-                ),
-              ])
-          .toList(),
-    );
-  }
+                  TypeClothingList(clothingList: entry.value),
+                ])
+            .toList(),
+      );
 }
